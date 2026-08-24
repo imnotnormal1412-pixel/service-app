d64ce45bdeb56bf87a3b9bd9bed6c8abc41f1480
imnotnormal1412-pixel <imnotnormal1412@gmail.com>
2026-08-24 21:33:09 +0300

    Change download file extension from .xls to .xlsx

--- app.py
+++ app.py
@@ -145,8 +145,8 @@ with st.expander("🔒 Панель хоста (Історія всіх чекі
             st.download_button(
                 label="📥 Завантажити повну історію чеків файлом",
                 data=history_data,
-                file_name="istoriya_chekiv.xls",
-                mime="text/plain"
+                file_name="istoriya_chekiv.xlsx",
+                mime="xlsx/plain"
             )
             st.text(history_data)
         else:
